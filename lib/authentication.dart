55461bd428a2d7b1ee9f76a5664088ecd9cd66fa
lmarquar <leon.marquardt@gmx.de>
2025-05-21 13:35:43 +0200

    started working on firebase_auth with: https://www.bacancytechnology.com/blog/email-authentication-using-firebase-auth-and-flutter

--- lib/authentication.dart
+++ lib/authentication.dart
@@ -1,5 +1,6 @@
-import 'package:firebase_auth/firebase_auth.dart';
 /**
+ import 'package:firebase_auth/firebase_auth.dart';
+
 class AuthenticationHelper {
 final FirebaseAuth _auth = FirebaseAuth.instance;
 get user => _auth.currentUser;
