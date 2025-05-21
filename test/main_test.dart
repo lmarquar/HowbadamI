01b72edd9087830c0a63617049efe9c69070d286
lmarquar <leon.marquardt@gmx.de>
2025-05-21 13:32:15 +0200

    started working on firebase_auth with: https://www.bacancytechnology.com/blog/email-authentication-using-firebase-auth-and-flutter

--- test/main_test.dart
+++ test/main_test.dart
@@ -3,11 +3,7 @@ import 'package:howbadami/main.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('Counter value should be incremented', () {
-    final counter = Counter();
+  test('testingtesting', () {
 
-    counter.increment();
-
-    expect(counter.value, 1);
   });
 }
