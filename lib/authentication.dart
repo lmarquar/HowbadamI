1461a278f278a2484d48cdeb85c2f86aece372ad
lmarquar <leon.marquardt@gmx.de>
2025-06-09 16:08:59 +0200

    implemented not working authentication

--- lib/authentication.dart
+++ lib/authentication.dart
@@ -46,6 +46,7 @@ if (user == null) {
 }
 });
     **/
+library;
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
--- test/main_test.dart
+++ test/main_test.dart
@@ -1,5 +1,4 @@
 // Import the test package and Counter class
-import 'package:howbadami/main.dart';
 import 'package:test/test.dart';
 
 void main() {
